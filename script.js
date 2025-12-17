ba0a9877bd233369c58a79cb182438b648582bfa
C.M.Vidhya <vidhyamanju6@gmail.com>
2025-12-17 20:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const makeChange = (c) => {
   // your name here
+ c = Number(c);
+
+  const result = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  // quarters (25 cents)
+  result.q = Math.floor(c / 25);
+  c = c % 25;
+
+  // dimes (10 cents)
+  result.d = Math.floor(c / 10);
+  c = c % 10;
+
+  // nickels (5 cents)
+  result.n = Math.floor(c / 5);
+  c = c % 5;
+
+  // pennies (1 cent)
+  result.p = c;
+
+  return result;
 };
 
 // Do not the change the code below
